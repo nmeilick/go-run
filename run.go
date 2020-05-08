cebe7ebbdfe410376a9f431740074e681e393137
meilick <Norman.Meilick@mail.schwarz>
2020-05-08 13:36:40 +0200

    Add execution error to Result object for Run()

--- run.go
+++ run.go
@@ -3,6 +3,15 @@ package run
 // Run executed the given external command using default settings and returns
 // a Result containing exit status and captured output, or an error if execution
 // was unsuccesful or timed out.
-func Run(name string, args ...string) (*Result, error) {
-	return Command(name, args...).Run()
+func Run(name string, args ...string) *Result {
+	r, err := Command(name, args...).Run()
+	if err == nil {
+		return r
+	}
+
+	return &Result{
+		ExitCode: 255,
+		Stdout:   "",
+		Stderr:   err.Error(),
+	}
 }
